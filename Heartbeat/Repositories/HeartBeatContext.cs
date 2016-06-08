41795aa6ba03de716d65f99ba9ac9fddd6b7676b
AlexandruDudescu <alexdudescu@hotmail.com>
2016-06-08 22:21:44 +0300

    Add project files.

---
+++ Heartbeat/Repositories/HeartBeatContext.cs
@@ -0,0 +1,13 @@
+﻿using HeartBeat_Project.Models;
+using System.Data.Entity;
+
+namespace HeartBeat_Project.Repositories
+{
+    public class HeartBeatContext:DbContext
+    {
+        public DbSet<HeartBeat> HeartBeats { get; set; }
+        public  HeartBeatContext() : base("name=HeartBeatDbConnectionString")
+        {
+        }
+    }
+}
---
+++ Heartbeat/Repositories/IHeartBeatRepository.cs
@@ -0,0 +1,13 @@
+﻿using HeartBeat_Project.Models;
+using System.Collections.Generic;
+
+namespace HeartBeat_Project.Repositories
+{
+    interface IHeartBeatRepository
+    {
+        void AddHeartBeat(HeartBeat heartBeat);
+        HeartBeat GetLastHeartBeat();
+        List<HeartBeat> GetAllHeartBeats();
+        List<HeartBeat> GetLastNStatuses(int numberOfStatuses);
+    }
+}
