680006446a2dad317ed10de3946a2f6c2cc157c6
AlexandruDudescu <alexdudescu@hotmail.com>
2016-06-08 15:55:47 +0300

    Add project files.

---
+++ HeartBeat Project/Models/HeartBeat.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HeartBeat_Project.Models
+{
+    public class HeartBeat
+    {
+        [Key]
+        public int SampleID { get; set; }
+        public int HearBeatRate { get; set; }
+        public string SampleDate { get; set; }
+
+        public HeartBeat(int heartBeatRate, string sampleDate)
+        {
+            SampleDate = sampleDate;
+            HearBeatRate = heartBeatRate;
+        }
+
+        public HeartBeat()
+        {
+        }
+    }
+}
---
+++ HeartBeatConsoleClient/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Threading.Tasks;
+using HeartBeat_Project.Models;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace HeartBeatConsoleClient
+{
+    class Program
+    {
+        private static string connectionString;
+        private static string portName;
+
+        private static DateTime startTime = DateTime.Now;
+        private static TimeSpan spanTime = TimeSpan.FromSeconds(5);
+
+        static void Main(string[] args)
+        {
+            getData();
+
+            Console.WriteLine("================================================");
+            Console.WriteLine("Trying to connect using following data:\nConnection string: {0}\nArduino Bluetooth Com Port: {1}", connectionString, portName);
+            Console.WriteLine("================================================");
+
+            while (true)
+            {
+                while (DateTime.Now < startTime + spanTime) ;
+                HeartBeat heartBeat = new HeartBeat(13, DateTime.Now.ToString());
+                PostAsync(heartBeat).Wait();
+                startTime = DateTime.Now;
+            }
+
+        }
+
+        static void getData()
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"Data.txt");
+            connectionString = lines[0];
+            portName = lines[1];
+
+
+        }
+
+        static async Task PostAsync(HeartBeat heartBeat)
+        {
+            using (var client = new HttpClient())
+            {
+                try
+                {
+                    client.BaseAddress = new Uri(connectionString);
+                    client.DefaultRequestHeaders.Accept.Clear();
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                    //Http post
+                    HttpResponseMessage response = await client.PostAsJsonAsync("api/HeartBeat/AddNewHeartBeat", heartBeat);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine("HeartBeat Posted succesfully!");
+                        PrintPost(heartBeat);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Attempt unsuccesfull!\n");
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Attempt unsuccesfull!\n" + ex.Message);
+                }
+            }
+
+        }
+
+        static void PrintPost(HeartBeat heartBeat)
+        {
+            Console.WriteLine("Your heart beat rate: {0}\n{1}\n", heartBeat.SampleDate, heartBeat.SampleDate);
+        }
+    }
+}
